97ba4822dfd853cf83a646956ac6295aa4e0fca9
jegors jeskovs <jegorsj@gmail.com>
2020-01-23 15:10:36 -0700

    list item is talking to the app

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
+  @Input() recipe: Recipe; // here we loading the Model Recipe and pass it along to a single recipe that we send to recipe-item.component
 
   constructor() { }
 
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,9 @@ import { Recipe } from "../recipe.model"
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe ("A test Recipe", "This is a simple test", "https://cdn.theatlantic.com/assets/media/img/mt/2016/08/badfood/lead_720_405.jpg?mod=1533691839" )
+    new Recipe ("A test Recipe", "This is a simple test", "https://cdn.theatlantic.com/assets/media/img/mt/2016/08/badfood/lead_720_405.jpg?mod=1533691839" ),
+    
+    new Recipe ("A test Recipe 2", "This is not a simple test", "https://cdn.theatlantic.com/assets/media/img/mt/2016/08/badfood/lead_720_405.jpg?mod=1533691839" )
   ];
 
   constructor() { }
