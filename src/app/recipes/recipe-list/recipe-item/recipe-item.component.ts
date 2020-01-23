96bab1a4a62de4419343fa414997c36215c07bd1
jegors jeskovs <jegorsj@gmail.com>
2020-01-23 16:40:23 -0700

    recipe detail added

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../../recipe.model';
 
 @Component({
@@ -8,6 +8,15 @@ import { Recipe } from '../../recipe.model';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe; // here we loading the Model Recipe and pass it along to a single recipe that we send to recipe-item.component
+  
+  @Output() recipeSelected = new EventEmitter<void>();
+
+
+  onSelected(){
+    this.recipeSelected.emit()
+  }
+
+
 
   constructor() { }
 
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from "../recipe.model"
+
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,11 +8,18 @@ import { Recipe } from "../recipe.model"
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  
   recipes: Recipe[] = [
     new Recipe ("A test Recipe", "This is a simple test", "https://cdn.theatlantic.com/assets/media/img/mt/2016/08/badfood/lead_720_405.jpg?mod=1533691839" ),
     
     new Recipe ("A test Recipe 2", "This is not a simple test", "https://cdn.theatlantic.com/assets/media/img/mt/2016/08/badfood/lead_720_405.jpg?mod=1533691839" )
   ];
+
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
+  }
 
   constructor() { }
 
